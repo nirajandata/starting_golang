23960e3e55f9aa79dd08be4504be192e3409270c
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-12-02 14:37:29 +0545

    Create variadic_function.go

---
+++ variadic_function.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(nums ...int) {
+	fmt.Println("nums are ", nums)
+	total := 0
+	for _, num := range nums {
+		fmt.Println(num)
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	sum(1, 2, 3)
+	sum(1, 2, 23, 3, 3, 3, 3, 3, 3)
+}
